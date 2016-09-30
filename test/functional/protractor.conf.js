f0ec5cd60d2741a62ff445ca6987436500a6f554
David Epely <esion99@gmail.com>
2016-09-30 12:19:35 +0200

    chore(test): add multiCapabilities browsers for functional tests
    + fix branch in saucelabs job name
    + add browsers : IE and safari (inactive)

--- test/functional/protractor.conf.js
+++ test/functional/protractor.conf.js
@@ -21,14 +21,36 @@ const cfg = {
 };
 
 if (!!process.env.SAUCE_USERNAME && !!process.env.SAUCE_ACCESS_KEY) {
+  const branch = (process.env.TRAVIS_PULL_REQUEST_BRANCH.length > 0) ?
+    process.env.TRAVIS_PULL_REQUEST_BRANCH :
+    process.env.TRAVIS_BRANCH;
+
   cfg.sauceUser = process.env.SAUCE_USERNAME;
   cfg.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  cfg.capabilities = {
-    browserName: 'firefox',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    build: process.env.TRAVIS_JOB_NUMBER,
-    name: `CaliOpen e2e - ${process.env.TRAVIS_BRANCH}`,
-  };
+  cfg.multiCapabilities = [
+    {
+      browserName: 'firefox',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_JOB_NUMBER,
+      name: `CaliOpen e2e - ${branch}`,
+    },
+    // {
+    //   browserName: 'Internet Explorer',
+    //   plateform: 'Windows 10',
+    //   version: '11.103',
+    //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    //   build: process.env.TRAVIS_JOB_NUMBER,
+    //   name: `CaliOpen e2e - ${branch}`,
+    // },
+    // {
+    //   browserName: 'Safari',
+    //   plateform: 'OS X 10.11',
+    //   version: '9.0',
+    //   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    //   build: process.env.TRAVIS_JOB_NUMBER,
+    //   name: `CaliOpen e2e - ${branch}`,
+    // },
+  ];
 }
 
 exports.config = cfg;
